b200957170ec0b994335b5a476f1d7d309cf0b39
Nick <xvip126@126.com>
2016-10-14 17:15:38 +0800

    Init

---
+++ Dyna/src/com/example/dynamic/callback/DialogCallback.java
@@ -0,0 +1,10 @@
+package com.example.dynamic.callback;
+/** 
+ ** FBI WARNING * MAGIC * DO NOT TOUCH **
+ ** CREATED BY NICK @ 2016年10月12日 下午7:00:44 
+ */
+
+public interface DialogCallback {
+	void onConfirm();
+	void onCancel();
+}
---
+++ Dyna/src/com/example/dynamic/impl/Dynamic.java
@@ -0,0 +1,33 @@
+package com.example.dynamic.impl;
+
+import com.example.dynamic.callback.DialogCallback;
+
+import android.app.Activity;
+
+/** 
+ ** FBI WARNING * MAGIC * DO NOT TOUCH **
+ ** CREATED BY NICK @ 2016年10月12日 下午1:52:42 
+ */
+
+public class Dynamic {
+	private static Activity activity;
+
+	public static void init(Activity activity) {
+	}
+
+	public static void showBanner() {
+	}
+
+	private static String result = "";
+
+	public static String showDialog(final DialogCallback callback) {
+		return null;
+	}
+
+	public static void destory() {
+	}
+
+	private static void safeToast(final String msg) {
+	};
+
+}
